393927dc7f4dd4e3fce43a8197ed5d8eac509cef
seedback <seedback2@gmail.com>
2022-02-09 17:09:25 +0100

    Initial commit

---
+++ include/gameObjects/InteractableObject.hh
@@ -0,0 +1,35 @@
+/*
+* _____________________________
+*    _____ ____   ____ _____   
+*   |_   _| __ ) / ___| ____|  
+*     | | |  _ \| |  _|  _|    
+*     | | | |_) | |_| | |___   
+*     |_| |____/ \____|_____|  
+* _____________________________
+*/
+
+/*
+	Class definition for the basic interactable object class.
+	This class is used for all objects that will react to one or more
+		dynamically generated commands consisting of a command-word and one or
+		more descriptors.
+	Is not meant to be used by itself, but to be inherited from. Though items
+		that can be interacted with but with no other pre-defined behaviour other
+		than a description, this can be used.
+	OBS: Objects of this class cannot be added to an inventory.
+		For that see the ObtainableItems class
+*/
+
+
+#include <string>
+#include <vector>
+
+#include "command/command.h"
+
+class InteractableObjects {
+	public:
+
+	private:
+		std::string name = "";
+		std::vector<Command> commands
+};
