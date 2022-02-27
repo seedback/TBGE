1302b28954d50888039a4e21f3069b44ba359ae4
seedback <seedback2@gmail.com>
2022-02-27 19:50:54 +0100

    Update: global_singleton class
    Update the global singleton class to be complete for the current feature-set and also contain complete Doxygen documentation

--- src/lib/global/global_singleton.hh
+++ src/lib/global/global_singleton.hh
@@ -24,18 +24,47 @@
 
 #include "toupper_string/toupper_string.hh"
 
+/**
+ * @todo Do something --Seedback
+ */
+
 namespace tbge {
 namespace global {
 
+/**
+ * @brief A global singleton holding, among other things, master lists of all
+ *          objects used in any given game.
+ * 
+ * @details
+ *   This is to help deal with memory leaks as most objects are accessed
+ *   through pointers so they can be reused elsewhere to save space.@n
+ *   As this is a singleton class you cannot make new objects of the class, but
+ *   you can access the one single existing instance through
+ *   GlobalSingleton::get_instance().
+ */
 class GlobalSingleton {
  public:
-  static GlobalSingleton& getInstance() {
+ /**
+  * @brief Get @b the instance of the GlobalSingleton.@n
+  *   This allways returns the same instance of GlobalSingleton so only one
+  *   object of this class can ever exist.
+  * 
+  * @return @a The instance of GlobalSingleton.@n
+  *           (there can only ever be one)
+  */
+  static GlobalSingleton& get_instance() {
     static GlobalSingleton instance;  // Guaranteed to be destroyed.
-                        // Instantiated on first use.
+                                      // Instantiated on first use.
     return instance;
   }
 
+
+  /// @brief @b Deleted to avoid having multiple instances of the
+  /// GlobalSingleton.
   GlobalSingleton(GlobalSingleton const&) = delete;
+
+  /// @brief @b Deleted to avoid having multiple instances of the
+  /// GlobalSingleton.
   void operator=(GlobalSingleton const&) = delete;
   // Note: Scott Meyers mentions in his Effective Modern
   //       C++ book, that deleted functions should generally
@@ -43,89 +72,52 @@ class GlobalSingleton {
   //       due to the compilers behavior to check accessibility
   //       before deleted status
 
-  /**
-  * @brief Setter for command_word_aliases_. Ensures that all entries (both
-  *          command-words and command-actions) are all-upper-case.
-  * @param command_word_aliases std::multimap containing command-word - alias
-  *          pairs to replace the current command_word_aliases_ with.
-  */
+  /// @brief Setter for @ref command_word_aliases_.
   void command_word_aliases(
-      std::multimap<std::string, std::string> command_word_aliases) {
-    std::multimap<std::string, std::string> new_multimap;
-    // Iterates through command_word_aliases with the iterator it
-    for (std::multimap<std::string, std::string>::iterator it =
-         command_word_aliases.begin();
-         it != command_word_aliases.end();
-         ++it) {
-      new_multimap.emplace(shf::toupper_string(it->first),
-                           shf::toupper_string(it->second));
-    }
-    this->command_word_aliases_ = new_multimap;
-  }
+      std::multimap<std::string, std::string> command_word_aliases);
 
   /**
-  * @brief Getter for command_word_aliases_.
-  * @return Complete std::multimap of all command-word - alias pairs.
+  * @brief Getter for @ref command_word_aliases_.
+  * @return Complete `std::multimap<std::string, std::string>` of all
+  *           @a command-word - @a alias pairs.
   */
   std::multimap<std::string, std::string> command_word_aliases() {
-    return command_word_aliases_;
+    return this->command_word_aliases_;
   }
 
   /**
-  * @brief Adds a cpmmand-word - alias pair to the list over such pairs
-  * @param command_word std::string defining the command-word the
-  *          alias belongs to
-  * @param alias std::string defining the alias the command-word belongs to
+  * @brief Adds a @a command-word - @a alias pair @ref command_word_aliases_
+  * @param command_word An @c std::string defining the @a command_word the
+  *          @a alias belongs to.
+  * @param alias An @c std::string defining the @a alias the @a command-word
+  *          belongs to.
   */
   void add_command_word_alias(std::string command_word, std::string alias) {
-    command_word_aliases_.emplace(
-        shf::toupper_string(command_word),
-        shf::toupper_string(alias));
+    this->command_word_aliases_.emplace(shf::toupper_string(command_word),
+                                  shf::toupper_string(alias));
   }
 
-  /**
-  * @brief Finds all aliases for a given command-word
-  *          and returns it as a vector.
-  * @param command_word std::string defining what command-word's aliases is
-  *          to be returned.
-  * @return A vector of all aliases of the given command-word.
-  */
-  std::vector<std::string> get_command_words_by_alias(
-      std::string alias) {
-    int length;
-    std::pair<std::multimap<std::string, std::string>::iterator,
-              std::multimap<std::string, std::string>::iterator> range;
-    std::vector<std::string> command_words;
-
-    alias = shf::toupper_string(alias);
-
-    length = command_word_aliases_.count(alias);
-
-    if (length) {
-      command_words.reserve(length);
-      range = command_word_aliases_.equal_range(alias);
-      for (std::multimap<std::string, std::string>::iterator it = range.first;
-           it != range.second; ++it) {
-        command_words.push_back(it->second);
-      }
-    }
-    return command_words;
-  }
+  /// @brief Finds all @a command-words for a given @a alias
+  ///          and returns them as a @c std::vector<std::string>.
+  std::vector<std::string> get_command_words_by_alias(std::string alias);
 
  private:
-  GlobalSingleton() {}  // Constructor? (the {} brackets) are needed here.
+  /// @brief Private constructor to avoid multiple objects of GlobalSingleton.
+  GlobalSingleton() {}
+
+
+  /// @brief Private destructor to ensure the object is only ever destroyed at
+  ///          program termination.
   ~GlobalSingleton() {}
 
-  // std::vector<Command> command_master_list;
 
-  /**
-   * @brief Holds command-word - command-action pairs of std::strings
-   *          all in all-upper-case
-   */
+  /// @brief Holds @a command-word - @a alias pairs of @c std::strings
+  ///          all in all-upper-case.
+
   std::multimap<std::string, std::string> command_word_aliases_;
-};
 
-// GlobalSingleton& test =  GlobalSingleton::getInstance();
+  // std::vector<Command> command_master_list;
+};
 
 }  // namespace global
 }  // namespace tbge
