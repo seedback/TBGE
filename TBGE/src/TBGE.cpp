f001f202020d782a444585e56d1892c1f2b7340a
seedback <seedback2@gmail.com>
2022-10-06 13:12:54 +0200

    temp

--- TBGE/src/TBGE.cpp
+++ TBGE/src/TBGE.cpp
@@ -1,22 +1,67 @@
-// TBGE.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// TBGE.cpp : This file contains the 'main' function. Program execution begins
+// and ends there.
 //
 
 #include <iostream>
 
 #include "include\global\global_singleton.h"
 
-int main()
-{  
-  std::cout << "Hello World!" << std::endl << tbge::global::global.s << std::endl;
+int main() {
+  tbge::GameObject* parent = new tbge::GameObject("parent");
+  tbge::GameObject* child_1 = new tbge::GameObject("child_1");
+  tbge::GameObject* child_2 = new tbge::GameObject("child_2");
+  tbge::GameObject* child_3 = new tbge::GameObject("child_3");
+
+  std::vector<tbge::GameObject*> child_list;
+  child_list.push_back(child_1);
+  child_list.push_back(child_2);
+  child_list.push_back(child_2);
+  child_list.push_back(child_3);
+
+  parent->add_children(child_list);
+
+  std::cout << "Listing children of " << parent->get_full_name() << std::endl;
+  for (int i = 0; i < parent->get_children().size(); i++) {
+    std::cout << "  " << parent->get_children()[i]->get_full_name()
+              << std::endl;
+  }
+
+  std::cout << std::endl;
+  std::cout << "Listing parents:" << std::endl
+            << "  " << child_1->get_full_name() << " : "
+            << child_1->get_parent()->get_full_name() << std::endl
+            << "  " << child_2->get_full_name() << " : "
+            << child_2->get_parent()->get_full_name() << std::endl
+            << "  " << child_3->get_full_name() << " : "
+            << child_3->get_parent()->get_full_name() << std::endl;
+
+  parent->remove_child(child_2);
+
+  std::cout << std::endl;
+  std::cout << "Listing children of " << parent->get_full_name() << std::endl;
+  for (int i = 0; i < parent->get_children().size(); i++) {
+    std::cout << "  " << parent->get_children()[i]->get_full_name()
+              << std::endl;
+  }
+
+  std::cout << std::endl;
+  std::cout << "Listing parents:" << std::endl;
+  std::cout << "  " << child_1->get_full_name() << std::endl;
+  std::cout << "  " << child_2->get_full_name() << std::endl;
+  std::cout << "  " << child_3->get_full_name() << std::endl;
+
+  child_1->set_parent(child_3);
+
+  std::cout << std::endl;
+  std::cout << "Listing children of " << parent->get_full_name() << std::endl;
+  for (int i = 0; i < parent->get_children().size(); i++) {
+    std::cout << "  " << parent->get_children()[i]->get_full_name()
+              << std::endl;
+  }
+
+  std::cout << std::endl;
+  std::cout << "Listing parents:" << std::endl;
+  std::cout << "  " << child_1->get_full_name() << std::endl;
+  std::cout << "  " << child_2->get_full_name() << std::endl;
+  std::cout << "  " << child_3->get_full_name() << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
